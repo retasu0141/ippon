2dbee9a7e50a0866992b8e38881053d5b313cb7f
retasu0141 <47290741+retasu0141@users.noreply.github.com>
2021-01-06 03:00:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,9 +96,10 @@ def delta(id):
     cur.execute('SELECT * FROM db')
     for row in cur:
         if id in row[0]:
-            sql = 'delete from db where id = ?'
-            data = (row[0])
-            conn.cursor.execute(sql, data)
+            #sql = "delete from db where id = '{id}'".format(id=row[0])
+            #data = (row[0])
+            #cur.execute(sql, data)
+            cur.execute("delete from db where id = '{id}'".format(id=row[0]))
         else:
             pass
     return
